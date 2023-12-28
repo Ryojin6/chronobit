de5bd3a50667187b4bc2cb3f5e27aa82fa3b6424
Ryojin <ryojin.eth@gmail.com>
2023-12-29 09:48:30 +1000

    add

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -44,21 +44,23 @@
           transactions, we're weaving stories that transcend time and embrace
           the future of effortless sophistication."
         </p>
+
+        <p class="pt-6 text-a-beige">ChronoBit by ABCustomer.</p>
       </div>
       <div class="space-y-10 text-center md:w-2/3">
         <AtomAnimate
           position="right"
-          class="border-lab-orange flex flex-col justify-center space-y-2 border-b border-opacity-50 pb-6 text-8xl text-white md:flex-row md:justify-start md:space-y-0 xl:pb-10 xl:text-9xl 2xl:text-[10rem]"
+          class="border-lab-orange flex flex-col justify-center space-y-2 border-b border-opacity-50 pb-6 text-8xl text-white md:flex-row md:justify-start md:space-y-0 xl:pb-10 xl:text-9xl"
         >
           <span class="outline">600+</span>
           <span
             class="font-machina text-lab-pink text-2xl uppercase 2xl:text-3xl"
-            >So we should be able to put
+            >5 Star Reviews
           </span>
         </AtomAnimate>
         <AtomAnimate
           position="right"
-          class="border-lab-orange flex flex-col justify-center space-y-2 border-b border-opacity-50 pb-6 text-8xl text-white md:flex-row md:justify-start md:space-y-0 xl:pb-10 xl:text-9xl 2xl:text-[10rem]"
+          class="border-lab-orange flex flex-col justify-center space-y-2 border-b border-opacity-50 pb-6 text-8xl text-white md:flex-row md:justify-start md:space-y-0 xl:pb-10 xl:text-9xl"
         >
           <span class="outline">2200+</span>
           <span
@@ -68,7 +70,7 @@
         </AtomAnimate>
         <AtomAnimate
           position="right"
-          class="border-lab-orange flex flex-col justify-center space-y-2 border-b border-opacity-50 pb-6 text-8xl text-white md:flex-row md:justify-start md:space-y-0 xl:pb-10 xl:text-9xl 2xl:text-[10rem]"
+          class="border-lab-orange flex flex-col justify-center space-y-2 border-b border-opacity-50 pb-6 text-8xl text-white md:flex-row md:justify-start md:space-y-0 xl:pb-10 xl:text-9xl"
         >
           <span class="outline">100k+</span>
           <span
@@ -78,7 +80,7 @@
         </AtomAnimate>
         <AtomAnimate
           position="right"
-          class="border-lab-orange flex flex-col justify-center space-y-2 border-b border-opacity-50 pb-6 text-8xl text-white md:flex-row md:justify-start md:space-y-0 xl:pb-10 xl:text-9xl 2xl:text-[10rem]"
+          class="border-lab-orange flex flex-col justify-center space-y-2 border-b border-opacity-50 pb-6 text-8xl text-white md:flex-row md:justify-start md:space-y-0 xl:pb-10 xl:text-9xl"
         >
           <span class="outline">$20k+</span>
           <span
@@ -92,7 +94,9 @@
 
     <AtomSection id="team" tp="large" bp="large" class="bg-lab-navy">
       <MoleculeTitle title="Who We Are" class="text-white" />
-      <AtomInner class="grid gap-12 md:grid-cols-2 xl:grid-cols-4">
+      <div
+        class="mx-auto grid max-w-4xl justify-center gap-12 md:grid-cols-2 xl:grid-cols-2"
+      >
         <AtomAnimate
           position="left"
           class="shadow-white bg-lab-navy rounded-3xl font-bold text-white shadow-2xl transition-all duration-1000 ease-in-out xl:p-10"
@@ -107,34 +111,7 @@
             />
           </div>
         </AtomAnimate>
-        <AtomAnimate
-          position="left"
-          class="shadow-white bg-lab-navy rounded-3xl font-bold text-white shadow-2xl transition-all duration-1000 ease-in-out xl:p-10"
-        >
-          <div class="flex flex-col items-center py-10 text-center">
-            <div class="mx-auto mb-10 h-48 w-48 rounded-full bg-a-beige" />
-            <h4 class="textMedium text-lab-pink">Lala</h4>
-            <div class="textSmall pt-4">CTO</div>
-            <AtomIconTwitter
-              link="#"
-              class="text-lab-pink hover:text-lab-lightpink mt-5"
-            />
-          </div>
-        </AtomAnimate>
-        <AtomAnimate
-          position="left"
-          class="shadow-white bg-lab-navy rounded-3xl font-bold text-white shadow-2xl transition-all duration-1000 ease-in-out xl:p-10"
-        >
-          <div class="flex flex-col items-center py-10 text-center">
-            <div class="mx-auto mb-10 h-48 w-48 rounded-full bg-a-beige" />
-            <h4 class="textMedium text-lab-pink">Loremipsum</h4>
-            <div class="textSmall pt-4">CMO, Partner</div>
-            <AtomIconTwitter
-              link=""
-              class="text-lab-pink hover:text-lab-lightpink mt-5"
-            />
-          </div>
-        </AtomAnimate>
+
         <AtomAnimate
           position="left"
           class="shadow-white bg-lab-navy rounded-3xl font-bold text-white shadow-2xl transition-all duration-1000 ease-in-out xl:p-10"
@@ -146,23 +123,20 @@
             <AtomIconTwitter link="https://twitter.com/Ryojn6" class="mt-5" />
           </div>
         </AtomAnimate>
-      </AtomInner>
+      </div>
     </AtomSection>
     <AtomSection
       tp="large"
       bp="large"
       class="bg-lab-pink flex h-screen items-center justify-center overflow-hidden"
     >
-      <AtomInner class="relative">
-        <AtomAnimate
-          class="relative z-10 mx-auto h-full max-w-5xl"
-          position="left"
-        >
-          <h2 class="textXLarge relative z-10 font-bold uppercase text-black">
+      <AtomInner class="relative grid items-center md:grid-cols-2">
+        <div class="relative z-10 mx-auto max-w-5xl" position="left">
+          <h2 class="textXLarge relative z-10 font-bold uppercase text-a-beige">
             All you <br />have to do
             <span class="font-machina block">is say hey!</span>
           </h2>
-          <AtomAnimate class="mt-10 flex w-full items-center space-x-6">
+          <div class="mt-10 flex w-full items-center space-x-6">
             <a href="#_" class="group relative inline-block text-lg">
               <span
                 class="relative z-10 block overflow-hidden rounded-lg border-2 border-gray-900 px-5 py-3 font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out group-hover:text-white"
@@ -197,17 +171,20 @@
                 data-rounded="rounded-lg"
               />
             </a>
-          </AtomAnimate>
-        </AtomAnimate>
+          </div>
+        </div>
         <div
           v-aos
-          class="pin-y-center absolute right-10 h-[70rem] w-[70rem] rounded-full transition-all duration-[2000ms] ease-in-out"
-          aos-from="opacity-0 shadow-xl"
-          aos-to="opacity-100 shadow-none  bg-a-beige"
+          class="h-full w-full max-w-xl rounded-full transition-all duration-[2000ms] ease-in-out"
         >
-          <div
-            class="pin-center absolute mx-auto h-[35rem] w-[35rem] rounded-full bg-[#1b1b1b]"
-          />
+          <svg viewBox="0 0 197.94 257" class="w-full fill-current">
+            <g id="PRINT">
+              <path
+                d="M196.96 38.72c-11.87 10.74-23.79 21.53-36.05 32.62-13.43-15.62-29.59-24.61-50.38-23.49-14.51.78-27.37 5.32-37.53 16.14-20.09 21.39-19.99 60.69.32 81.87 12.34 12.86 27.94 16.77 45.15 15.64 14.76-.97 20.61-4.17 42.8-22.97 11.87 10.74 23.8 21.52 36.67 33.16-6.58 5.88-12.44 12.05-19.19 17-23.43 17.16-50.2 22.24-78.6 20.11-26.41-1.97-50.25-10.77-69.19-29.75C1.57 149.58-6.25 113.71 4.77 74.52 15.75 35.46 43.26 12.23 82.43 3.44c29.28-6.57 57.98-4.01 85.04 10.03 12.12 6.29 24.45 16.97 29.49 25.25ZM182.26 235.56c0 6.39.12 12.52-.13 18.64-.04.92-1.63 2.01-2.74 2.59-.79.41-1.96.1-2.96.1H15.64v-21.33h166.61Z"
+                class="cls-1"
+              />
+            </g>
+          </svg>
         </div>
       </AtomInner>
     </AtomSection>
