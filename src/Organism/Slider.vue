270888f6ae0e21a50a581bf60630311163bd4f20
Ryojin <ryojin.eth@gmail.com>
2024-01-13 15:06:52 +1000

    ss

--- src/Organism/Slider.vue
+++ src/Organism/Slider.vue
@@ -13,7 +13,6 @@
       <slide> <img src="/2.jpg" class="w-full" alt="" /> </slide>
       <slide> <img src="/3.jpg" class="w-full" alt="" /> </slide>
       <slide> <img src="/4.jpg" class="w-full" alt="" /> </slide>
-      <slide> <img src="/5.jpg" class="w-full" alt="" /> </slide>
     </hooper>
 
     <div
